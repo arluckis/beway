05be35127c16ad5e7fac501b022d3d0348a60455
RenerTaveira <142452315+RenerTaveira@users.noreply.github.com>
2024-07-23 09:10:49 -0300

    Update Viagem.hpp

--- Viagem.hpp
+++ Viagem.hpp
@@ -1,33 +1,14 @@
 #ifndef VIAGEM_HPP
 #define VIAGEM_HPP
 
-#include <iostream>
-#include <vector>
-#include "cidade.hpp"        // Supondo que exista um arquivo cidade.hpp com a definição de Cidade
-#include "transporte.hpp"    // Supondo que exista um arquivo transporte.hpp com a definição de Transporte
-#include "passageiro.hpp"    // Supondo que exista um arquivo passageiro.hpp com a definição de Passageiro
-#include "estado_viagem.hpp" // Supondo que exista um arquivo estado_viagem.hpp com a definição de EstadoViagem
+#include <iostream>          // Inicializador do programa
+#include <vector>            // Incluir a biblioteca vetor, para poder utilizar vetores durante a programação
+#include "city.hpp"          // Pegar informações referente a um arquivo chamado de city.hpp que possui definido a variável City(Cidade)
+#include "transporte.hpp"    // Pegar informações referente a um arquivo chamado de transport.hpp que possui definido a variável Transport(Transporte)
+#include "passageiro.hpp"    // Pegar informações referente a um arquivo chamado de passenger.hpp que possui definido a variável Passenger(Passageiro)
+#include "estado_viagem.hpp" // Verificar se o estado da viagem virá de um outro arquivo ou neste!!!!!
 
-class Viagem {
-private:
-    const Cidade& origem;
-    const Cidade& destino;
-    const Transporte& transporte;
-    std::vector<const Passageiro*> passageiros;
-    EstadoViagem estado;
 
-public:
-    Viagem(const Cidade& origem, const Cidade& destino, const Transporte& transporte);
 
-    const Cidade& getOrigem() const;
-    const Cidade& getDestino() const;
-    const Transporte& getTransporte() const;
-    const std::vector<const Passageiro*>& getPassageiros() const;
-    EstadoViagem getEstado() const;
-
-    void registrarPartida();
-    void registrarChegada();
-    void avancarTempo(double horas);
-};
 
 #endif // VIAGEM_HPP
