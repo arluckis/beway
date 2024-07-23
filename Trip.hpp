e053a3dde0b6d80f9420a0bff7a0044d8aee6ed7
RenerTaveira <142452315+RenerTaveira@users.noreply.github.com>
2024-07-23 11:20:43 -0300

    Update Trip.hpp

--- Trip.hpp
+++ Trip.hpp
@@ -1,8 +1,10 @@
-#ifndef TRIP_HPP
+e#ifndef TRIP_HPP
 #define TRIP_HPP
 
 #include <iostream>          // Inicializador do programa em C++
 #include <vector>            // Incluir a biblioteca vetor, para poder utilizar vetores durante a programação
+#include <cmath>             // Para poder utilizar a função ceil()
+#include <string>            // Incluir a biblioteca string,
 #include "city.hpp"          // Pegar informações referente a um arquivo chamado de city.hpp que possui definido a variável City(Cidade)
 #include "transporte.hpp"    // Pegar informações referente a um arquivo chamado de transport.hpp que possui definido a variável Transport(Transporte)
 #include "passageiro.hpp"    // Pegar informações referente a um arquivo chamado de passenger.hpp que possui definido a variável Passenger(Passageiro)
@@ -21,6 +23,11 @@ private:
 Public:
   // Definindo o construtor para Trip: variável transport, city origin e city destiny devem ser informadas e demais variáveis tem que ser inicializa
   Trip(Transport* transport, City* origin_, City* destinyCity) : transportOfTrip(transport), origin(originCity), destiny(destinyCity), next(nullptr), timeInTraffic(0), inProgress(false) {}
+  //Calcular o tempo estimado com os dados da distância percorrida, velocidade média e tempo de descando do transporte
+  double distance = calculateDistance(origin, destinyCity);
+  double numberOfRests = ceil(distance/ distanceTimeOfRest);
+  double averageSpeed = transport->getAverageSpeed();
+  double estimatedTime = ((distance / averageSpeed) + (numberOfRests * timeOfRest))
 
 
   // Método utilizado para dar início a uma viagem
@@ -42,7 +49,10 @@ Public:
     std::cout << "Origin City: " << origin->getName() << std::endl;
     std::cout << "Destiny City: " << destiny->getName() << std::endl;
     std::cout << "Time in traffic: " << timeInTraffic << "hours" << std::endl;
-    for (auto)
+    for passenger {
+      std::cout << 
+      }
+    std::cout << 
   }
 
   // Retorna a variável inProgress que irá definir se a viagem está em progresso ou não
