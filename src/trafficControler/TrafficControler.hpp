196449d954154f56cf4da3c734b08f8df78cbf6d
RenerTaveira <142452315+RenerTaveira@users.noreply.github.com>
2024-07-30 11:27:59 -0300

    Update TrafficControler.hpp

--- src/trafficControler/TrafficControler.hpp
+++ src/trafficControler/TrafficControler.hpp
@@ -3,12 +3,13 @@
 
 #include <iostream>          // Inicializador do programa em C++
 #include <vector>            // Incluir a biblioteca vetor, para poder utilizar vetores durante a programação
-#include <string>            // Incluir a biblioteca string
-#include "city.hpp"          // Pegar informações referente a um arquivo chamado de city.hpp que possui definido a variável City(Cidade)
-#include "route.hpp"         // Pegar informações referente a um arquivo chamado de route.hpp que possui definido a variável Route(rotas)
-#include "transporte.hpp"    // Pegar informações referente a um arquivo chamado de transport.hpp que possui definido a variável Transport(Transporte)
-#include "passageiro.hpp"    // Pegar informações referente a um arquivo chamado de passenger.hpp que possui definido a variável Passenger(Passageiro)
-#include "passageiro.hpp"    // Pegar informações referente a um arquivo chamado de trip.hpp que possui definido a variável Trip(Viagem)
+#include <string>            // Incluir a biblioteca string, para a criação e manipulação de cadeia de caracteres
+#include <algorithm>         // Incluir a biblioteca algorithm, para fornecer uma série de algoritmos genéricos e funções de utilidade para operações comuns
+#include "../city/city.hpp"          // Pegar informações referente a um arquivo chamado de city.hpp que possui definido a variável City(Cidade)
+#include "../route/route.hpp"         // Pegar informações referente a um arquivo chamado de route.hpp que possui definido a variável Route(rotas)
+#include "../transport/transport.hpp"    // Pegar informações referente a um arquivo chamado de transport.hpp que possui definido a variável Transport(Transporte)
+#include "../passenger/passenger.hpp"    // Pegar informações referente a um arquivo chamado de passenger.hpp que possui definido a variável Passenger(Passageiro)
+#include "../trip/trip.hpp"    // Pegar informações referente a um arquivo chamado de trip.hpp que possui definido a variável Trip(Viagem)
 
 using namespace std;         // Isenta o uso do std durante o programa
 
@@ -19,6 +20,42 @@ private:
   std::vector <Transport*> transports;
   std::vector <Passenger*> passengers;
   std::vector <Trip*> trips;
+
+// Função para procurar se o nome da cidade existe no ponteiro city, caso não encontre retorna "não encontrado"
+City* findCityByName(const string& name) const{
+  for (City* city : cities) {
+    if (city->getName() == name) {
+      return city;
+    }
+  }
+  return nullptr;
+}
+
+// Função para procurar uma rota direta ou indireta, caso não encontre retorna "não encontrado"
+Route* findRouteByCities(City* origin, city* destination) const{
+  for (Route* route : routes) { // Verifica rotas diretas entre a origem e o destino
+    if (route->getOrigin() == origin && route->getDestination() == destination) {
+      return route;
+    }
+  }
+
+  // Caso não seja encontrada nenhuma rota direta, será analisado as rotas indiretas
+  std::unordered_set <City*> visited;
+  std::queue<std::pair<City*, Route*>> toIndirectWay;
+
+  // Adiciona a cidade de origem ao caminho indireto
+  toIndirectWay.push({origin, nullptr})
+  visited.insert(origin);
+
+  while (!toIndirectWay.empty()) {
+    City* currentCity = toIndirectWay.front().first;
+    Route* lastRoute = toIndirectWay.front().second;
+    toIndirectWay.pop();
+
+    // Reali
+  }
+  return nullptr;
+}
 
 public:
   void cadastreCity(std::string name);
