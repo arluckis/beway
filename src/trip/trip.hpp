ad5ea77c58d2ccb3557588e5ba7bfb1b435fe621
RenerTaveira <142452315+RenerTaveira@users.noreply.github.com>
2024-07-30 10:33:14 -0300

    Update trip.hpp

--- src/trip/trip.hpp
+++ src/trip/trip.hpp
@@ -5,13 +5,13 @@
 #include <vector>            // Incluir a biblioteca vetor, para poder utilizar vetores durante a programação
 #include <cmath>             // Para poder utilizar a função ceil()
 #include <string>            // Incluir a biblioteca string
-#include "../city/city.hpp"              // Pegar informações referente a um arquivo chamado de city.hpp que possui definido a variável City(Cidade)
-#include "../transport/transport.hpp"    //Pegar informações referente a um arquivo chamado de transport.hpp que possui definido a variável Transport(Transporte)
-#include "../passenger/passenger.hpp"    // Pegar informações referente a um arquivo chamado de passenger.hpp que possui definido a variável Passenger(Passageiro)
+#include "../city/city.hpp"                // Pegar informações referente a um arquivo chamado de city.hpp que possui definido a variável City(Cidade)
+#include "../transport/transport.hpp"      //Pegar informações referente a um arquivo chamado de transport.hpp que possui definido a variável Transport(Transporte)
+#include "../passenger/passenger.hpp"      // Pegar informações referente a um arquivo chamado de passenger.hpp que possui definido a variável Passenger(Passageiro)
 
 using namespace std;         // Isenta o uso do std durante o seguinte programa
 
-class Trip { //Definindo a classe Trip(Viagem)
+class Trip { // Definindo a classe Trip (Viagem)
 private:
   Transport* transportOfTrip; 
   std::vector <Passenger*> passengers;
@@ -20,17 +20,22 @@ private:
   Trip* next;
   int timeInTraffic;
   bool inProgress;
+  double calculateDistance(City* origin, City* destiny){      // Valor inicial para a distância em 0.0 (pois é uma variável double)
+    return 0.0;
+  }
 
 public:
   // Definindo o construtor para Trip: variável transport, city origin e city destiny devem ser informadas e demais variáveis tem que ser inicializa
   Trip(Transport* transport, City* origin_, City* destinyCity) : transportOfTrip(transport), origin(originCity), destiny(destinyCity), next(nullptr), timeInTraffic(0), inProgress(false) {}
 
   //Calcular o tempo estimado com os dados da distância percorrida, velocidade média e tempo de descando do transporte
-  /*double distance = calculateDistance(origin, destinyCity);
-  double numberOfRests = ceil(distance/ distanceTimeOfRest);
-  double averageSpeed = transport->getAverageSpeed();
-  double estimatedTime = ((distance / averageSpeed) + (numberOfRests * timeOfRest))
-
+  void calculateEstimatedTime(){
+    double distance = calculateDistance(origin, destinyCity);          // Calcula a distância percorrida
+    double numberOfRests = ceil(distance/ distanceTimeOfRest);         // Números de descansos
+    double timeOfRest = transportOfTrip->getRestTime();                // Pega o tempo de descanso do transporte de outro arquivo
+    double averageSpeed = transportOfTrip->getAverageSpeed();          // Pega a velocidade média do transporte de outro arquivo
+    double estimatedTime = ((distance / averageSpeed) + (numberOfRests * timeOfRest))       // Tempo estimado
+  }
 
   // Método utilizado para dar início a uma viagem
   void beginTrip() {
@@ -40,11 +45,14 @@ public:
 
   // Método utilizado para adiantar a hora
   void advanceHour() {
-    TimeInTraffic++;
-    if (timeInTraffic >= estimatedTime) {
-      endTrip();    //Se o tempo em viagem for maior que o tempo estimado da viagem, a viagem tem que ser finalizada
+    if (inProgress) {
+      TimeInTraffic++;
+      if (timeInTraffic >= estimatedTime) {
+        endTrip();    // Se o tempo em viagem for maior que o tempo estimado da viagem, a viagem tem que ser finalizada
+      }
     }
   }
+
   // Mostra todos os detalhes da viagem (Cidade de origem, Cidade de destino, Tempo decorrido, passageiros na viagem e estado da viagem)
   void showStatus() {
     std::cout << "Trip details: " << std::endl;
@@ -62,6 +70,6 @@ public:
   bool isInProgress() const {
     return inProgress;
   }
-};*/
+};
 
 #endif // trip_hpp
