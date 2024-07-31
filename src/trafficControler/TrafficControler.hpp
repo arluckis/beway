6ba42d6baed5c1a98c02892f740a4880b7a1d3ee
RenerTaveira <142452315+RenerTaveira@users.noreply.github.com>
2024-07-31 14:01:13 -0300

    Update TrafficControler.hpp

--- src/trafficControler/TrafficControler.hpp
+++ src/trafficControler/TrafficControler.hpp
@@ -46,7 +46,7 @@ Route* findRouteByCities(City* origin, City* destination) const {
   std::queue<std::pair<City*, Route*>> toIndirectWay;
 
   // Adiciona a cidade de origem ao caminho indireto
-  toIndirectWay.push({origin, nullptr})
+  toIndirectWay.push({origin, nullptr});
   visited.insert(origin);
 
   while (!toIndirectWay.empty()) {
@@ -55,13 +55,13 @@ Route* findRouteByCities(City* origin, City* destination) const {
     toIndirectWay.pop();
 
     // Realiza averificação da cidade de origem e destino, e insere as cidades no caminho indireto (toIndirectWay)
-    for (Route* route : route) {
+    for (Route* route : routes) {
       if (route->getOrigin() == currentCity && visited.find(route->getDestination()) == visited.end()){
         if (route->getDestination() == destination) {
           return route;
         }
-        toIndirectWay.push({route->getDestination(), route})
-        visited.insert(route->getDestination())
+        toIndirectWay.push({route->getDestination(), route});
+        visited.insert(route->getDestination());
       }
     }
   }
@@ -111,12 +111,12 @@ public:
       std::cout << "- From " << route->getOrigin()->getName() << " to " << route->getDestiny()getName() << "(" << route->getType() << ", " << route->getDistance() << " km)" << std::endl;
     }
 
-    std::cout << "Cadastred Transports" << std::endl; // Informações sobre os transports
+    std::cout << "Cadastred Transports" << std::endl; // Informações sobre os transportes
      for (auto transport : transports) {
       std::cout << "* " << transport->getName() << " (Type: " << transport->getType() << ", Capacity: " << transport->getCapacity() << ", Speed: "  << transport->getSpeed() << "Km/h" << std::endl;
     }
     
-    std::cout << "Cadastred Passengers" << std::endl; // Informações sobre os transports
+    std::cout << "Cadastred Passengers" << std::endl; // Informações sobre os passageiros
     for (auto passenger : passengers) {
       std::cout << " (Current City: " << passenger->getCurrentCity()->getName() << ")" << std::endl;
     }
