0aae17db04eb6ca727105f53705d98154273c09b
RenerTaveira <142452315+RenerTaveira@users.noreply.github.com>
2024-08-05 09:13:19 -0300

    Update trip.cpp

--- src/trip/trip.cpp
+++ src/trip/trip.cpp
@@ -12,3 +12,42 @@ double calculateDistance(City* origin, City* destiny) {      // Valor inicial pa
     double averageSpeed = transportOfTrip->getAverageSpeed();          // Pega a velocidade média do transporte de outro arquivo
     double estimatedTime = ((distance / averageSpeed) + (numberOfRests * timeOfRest))       // Tempo estimado
   }
+
+ // Método utilizado para dar início a uma viagem
+  void beginTrip() {
+    inProgress = true;
+    timeInTraffic = 0;  //Verificar necessidade, pois já se inicia em 0
+  }
+
+  // Método utilizado para adiantar a hora
+  void advanceHour() {
+    if (inProgress) {
+      TimeInTraffic++;
+      if (timeInTraffic >= estimatedTime) {
+        endTrip();    // Se o tempo em viagem for maior que o tempo estimado da viagem, a viagem tem que ser finalizada
+      }
+    }
+  }
+
+// Método utilizado para alterar o estado da viagem para finalizado
+void endTrip() {
+  inProgress = false;
+}
+
+  // Mostra todos os detalhes da viagem (Cidade de origem, Cidade de destino, Tempo decorrido, passageiros na viagem e estado da viagem)
+  void showStatus() {
+    std::cout << "Trip details: " << std::endl;
+    std::cout << "Origin City: " << origin->getName() << std::endl;
+    std::cout << "Destiny City: " << destiny->getName() << std::endl;
+    std::cout << "Time in traffic: " << timeInTraffic << "hours" << std::endl;
+    std::cout << "Status: " << (inProgress? "In Progress" : "Not In Progress") << std::endl;
+    std::cout << "Passenger: " << std::endl;
+    for (Passenger* passenger : passengers) {
+      std::cout << " - " << passenger->getName() << std::endl;
+      }
+  }
+
+  // Retorna a variável inProgress que irá definir se a viagem está em progresso ou não
+  bool isInProgress() const {
+    return inProgress;
+  }
