05c461285978cfbcbf66b5facfb3ad7b4173ee92
RenerTaveira <142452315+RenerTaveira@users.noreply.github.com>
2024-08-05 09:05:17 -0300

    Update trip.cpp

--- src/trip/trip.cpp
+++ src/trip/trip.cpp
@@ -1,3 +1,14 @@
 #include "trip.hpp"
 
+double calculateDistance(City* origin, City* destiny) {      // Valor inicial para a distância em 0.0 (pois é uma variável double)
+    return 0.0;
+}
 
+//Calcular o tempo estimado com os dados da distância percorrida, velocidade média e tempo de descando do transporte
+  void Trip::calculateEstimatedTime() {
+    double distance = calculateDistance(origin, destinyCity);          // Calcula a distância percorrida
+    double numberOfRests = ceil(distance/ distanceTimeOfRest);         // Números de descansos
+    double timeOfRest = transportOfTrip->getRestTime();                // Pega o tempo de descanso do transporte de outro arquivo
+    double averageSpeed = transportOfTrip->getAverageSpeed();          // Pega a velocidade média do transporte de outro arquivo
+    double estimatedTime = ((distance / averageSpeed) + (numberOfRests * timeOfRest))       // Tempo estimado
+  }
