be439668d9aed59a6eadec852240ea92e3150477
RenerTaveira <142452315+RenerTaveira@users.noreply.github.com>
2024-08-05 09:22:16 -0300

    Update trip.hpp

--- src/trip/trip.hpp
+++ src/trip/trip.hpp
@@ -20,61 +20,29 @@ private:
   Trip* next;
   int timeInTraffic;
   bool inProgress;
-  double calculateDistance(City* origin, City* destiny){      // Valor inicial para a distância em 0.0 (pois é uma variável double)
-    return 0.0;
-  }
 
-  //Calcular o tempo estimado com os dados da distância percorrida, velocidade média e tempo de descando do transporte
-  void calculateEstimatedTime(){
-    double distance = calculateDistance(origin, destinyCity);          // Calcula a distância percorrida
-    double numberOfRests = ceil(distance/ distanceTimeOfRest);         // Números de descansos
-    double timeOfRest = transportOfTrip->getRestTime();                // Pega o tempo de descanso do transporte de outro arquivo
-    double averageSpeed = transportOfTrip->getAverageSpeed();          // Pega a velocidade média do transporte de outro arquivo
-    double estimatedTime = ((distance / averageSpeed) + (numberOfRests * timeOfRest))       // Tempo estimado
-  }
+  double calculateDistance(City* origin, City* destiny);      // Valor inicial para a distância em 0.0 (pois é uma variável double, somente declarar)
+   
+  void calculateEstimated();
 
 public:
   // Definindo o construtor para Trip: variável transport, city origin e city destiny devem ser informadas e demais variáveis tem que ser inicializadas
-  Trip(Transport* transport, City* origin_, City* destinyCity) : transportOfTrip(transport), origin(originCity), destiny(destinyCity), next(nullptr), timeInTraffic(0), inProgress(false), calculateEstimatedTime() {}
+  Trip(Transport* transport, City* origin_, City* destinyCity);
 
   // Método utilizado para dar início a uma viagem
-  void beginTrip() {
-    inProgress = true;
-    timeInTraffic = 0;  //Verificar necessidade, pois já se inicia em 0
-  }
+  void beginTrip();
 
   // Método utilizado para adiantar a hora
-  void advanceHour() {
-    if (inProgress) {
-      TimeInTraffic++;
-      if (timeInTraffic >= estimatedTime) {
-        endTrip();    // Se o tempo em viagem for maior que o tempo estimado da viagem, a viagem tem que ser finalizada
-      }
-    }
-  }
+  void advanceHour();
 
-// Método utilizado para alterar o estado da viagem para finalizado
-void endTrip() {
-  inProgress = false;
-}
+  // Método utilizado para alterar o estado da viagem para finalizado
+void endTrip();
 
   // Mostra todos os detalhes da viagem (Cidade de origem, Cidade de destino, Tempo decorrido, passageiros na viagem e estado da viagem)
-  void showStatus() {
-    std::cout << "Trip details: " << std::endl;
-    std::cout << "Origin City: " << origin->getName() << std::endl;
-    std::cout << "Destiny City: " << destiny->getName() << std::endl;
-    std::cout << "Time in traffic: " << timeInTraffic << "hours" << std::endl;
-    std::cout << "Status: " << (inProgress? "In Progress" : "Not In Progress") << std::endl;
-    std::cout << "Passenger: " << std::endl;
-    for (Passenger* passenger : passengers) {
-      std::cout << " - " << passenger->getName() << std::endl;
-      }
-  }
+  void showStatus();
 
   // Retorna a variável inProgress que irá definir se a viagem está em progresso ou não
-  bool isInProgress() const {
-    return inProgress;
-  }
+  bool isInProgress() const;
 };
 
 #endif // trip_hpp
